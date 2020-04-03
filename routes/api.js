acd392579532516210779cd34f04e40172cfd7a9
VitaliiLynnyk <linnik2101@gmail.com>
2020-04-03 19:07:09 +0200

    add checkIfValid

--- routes/api.js
+++ routes/api.js
@@ -14,28 +14,30 @@ const axios = require('axios');
 module.exports = (app) => {
   app.route('/api/stock-prices').get((req, res) => {
     const { stock, like } = req.query;
-    let promiseArray = [];
-    if (Array.isArray(stock)) {
-      stock.forEach((e) => {
-        promiseArray.push(e);
-      });
-    } else {
-      promiseArray.push(stock);
-    }
+    let promiseArray = Array.isArray(stock) ? [...stock] : [stock];
+
     promiseArray = promiseArray.map((stock) => getPrice(stock));
 
     Promise.all(promiseArray).then((results) => {
       if (results.length === 1) {
-        res.json({ stockData: { ...results[0], likes: +!!like } });
+        return res.json(checkIfValid(results[0], like));
       } else {
         let parsed = results.map((e) => {
-          return { stockData: { ...e, rel_likes: +!!like } };
+          return checkIfValid(e, like);
         });
-        res.json({ stockData: parsed });
+        return res.json({ stockData: parsed });
       }
     });
   });
 };
+
+function checkIfValid(requestRes, like) {
+  if (requestRes.price) {
+    return { stockData: { ...requestRes, likes: +!!like } };
+  } else {
+    return { stockData: { error: 'external source error', likes: +!!like } };
+  }
+}
 
 async function getPrice(stock) {
   const response = await axios.get(
